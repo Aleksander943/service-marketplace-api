26c197dc97c56774669d1f3caf1d8e11a803b8cb
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-04 09:47:16 -0300

    feat: criação de cadastro

---
+++ src/services/CreateUserService.ts
@@ -0,0 +1,34 @@
+import { prisma } from "../config/prisma";
+
+export const CreatedUserService = async (nome: string, email: string, password: string ) => {
+    
+    if(!email){
+       throw new Error("Email é obrigatório");
+    };
+
+    const UserExist = await prisma.user.findUnique({
+        where: {
+            email: email
+        }
+    });
+
+    if(UserExist){
+        throw new Error("Usuário já cadastrado");
+    };
+
+    const User = await prisma.user.create({
+        data: {
+            nome,
+            email,
+            password,
+        },
+        select:{
+            id: true,
+            name: true,
+            email: true,
+        }
+    });
+
+    return User
+};
+
