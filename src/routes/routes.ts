416e89b76f304515caf067ea749acb3f69050603
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-05 09:35:08 -0300

    feat: adição da nova rota de cadastro profissional

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,8 +1,11 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/CreateUserController";
+import { CreateProfissionalController } from "../controllers/CreateProfissinalController";
 
 const router = Router();
 
 router.post("/user", CreateUserController);
+
+router.post("/profissional", CreateProfissionalController);
 
 export {router};
