29915858f64db5e0fd1a1b9f15446adc682f51fa
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-07 14:00:45 -0300

    refactor: remoção de importações não utilizadas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,12 +1,8 @@
 import { Router } from "express";
 import { CreateUserController } from "../controllers/CreateUserController";
-import { CreateProfissionalController } from "../controllers/CreateProfissinalController";
 
 const router = Router();
 
 router.post("/users", CreateUserController);
-
-
-router.post("/profissional", CreateProfissionalController);
 
 export {router};
