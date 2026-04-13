0e3cf07ce9787b44bbfbd9462e2ca892323b8ad1
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-12 21:39:14 -0300

    feat: adição de novas rotas

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,10 +2,15 @@ import { Router } from "express";
 import 'dotenv/config';
 import { CreateUserController } from "../controllers/CreateUserController";
 import { AuthUserController } from "../controllers/AuthUserController";
+import { isAuthenticated } from "../middleware/isAuthenticated";
 
 const router = Router();
 
 router.post("/Cadastro", CreateUserController);
 router.post("/login", AuthUserController);
+
+router.post("/criarServicos", isAuthenticated)
+router.post("/agendamento", isAuthenticated)
+
 
 export {router};
