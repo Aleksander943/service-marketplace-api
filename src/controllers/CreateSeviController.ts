c4bb6d92125b3a30ec0ae8a17dbfcb569c562307
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-14 18:36:58 -0300

    feat: cadastro de serviço com o service

--- src/controllers/CreateSeviController.ts
+++ src/controllers/CreateSeviController.ts
@@ -1,3 +1,22 @@
-export const CadastroServServices = () => {
-    
-}
+import { Request, Response } from "express";
+import { CadastroServServices } from "../services/CreateServServices";
+
+export const CadastroServController = async (req: Request, res: Response) => {
+  const { title, description, price, category } = req.body;
+
+  const user_id = req.user_id;
+
+  try {
+    const resultado = await CadastroServServices(
+      title,
+      description,
+      price,
+      category,
+      user_id,
+    );
+    res.status(201).json(resultado);
+  } catch (err: any) {
+    console.log(err);
+    res.status(400).json({ message: err.message || "Error" });
+  }
+};
