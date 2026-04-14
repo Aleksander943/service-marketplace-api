5a23becfd5168ca69dcc643e3cadfa13c3f62bbf
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-14 18:39:48 -0300

    refactor: remove comentário e limpa linhas desnecessárias

--- src/controllers/CreateUserController.ts
+++ src/controllers/CreateUserController.ts
@@ -1,18 +1,15 @@
-import {CreatedUserService} from "../services/CreateUserService";
+import { CreatedUserService } from "../services/CreateUserService";
 
-import { Request,Response } from "express";
+import { Request, Response } from "express";
 
 export const CreateUserController = async (req: Request, res: Response) => {
-    const {nome, email, password, role} = req.body;
+  const { nome, email, password, role } = req.body;
 
-    try{
-        const result = await CreatedUserService(nome, email, password, role)
-        res.status(201).json(result)
-    } catch (err: any) {
+  try {
+    const result = await CreatedUserService(nome, email, password, role);
+    res.status(201).json(result);
+  } catch (err: any) {
     console.log(err); // <-- ADICIONE ISSO AQUI
     res.status(400).json({ message: err.message || "Error" });
-}
-	
+  }
 };
-
-
