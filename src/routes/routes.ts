f39ab148a3a34f5d6e8883969560b785944196c3
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-14 18:37:17 -0300

    chore: ajusta rotas protegidas de serviços

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import 'dotenv/config';
 import { CreateUserController } from "../controllers/CreateUserController";
 import { AuthUserController } from "../controllers/AuthUserController";
+import { CadastroServController } from "../controllers/CreateSeviController";
 import { isAuthenticated } from "../middleware/isAuthenticated";
 
 const router = Router();
@@ -9,8 +10,8 @@ const router = Router();
 router.post("/Cadastro", CreateUserController);
 router.post("/login", AuthUserController);
 
-router.post("/criarServicos", isAuthenticated)
-router.post("/agendamento", isAuthenticated)
+router.post("/criarServicos", isAuthenticated,CadastroServController)
+// router.post("/agendamento", isAuthenticated)
 
 
 export {router};
