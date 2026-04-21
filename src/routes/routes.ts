c1281d5fba6123c003d9fff860c2fb107bd87957
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-21 12:44:21 -0300

    feat: criar rota POST /agendamento com middleware de autenticação

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -5,14 +5,16 @@ import { AuthUserController } from "../controllers/AuthUserController";
 import { CadastroServController } from "../controllers/CreateSeviController";
 import { isAuthenticated } from "../middleware/isAuthenticated";
 import { ListServicesController } from "../controllers/ListServicesController";
+import { AppointmentController } from "../controllers/AppointmentController";
 
 const router = Router();
 
 router.post("/cadastro", CreateUserController);
 router.post("/login", AuthUserController);
+router.post("/Servicos", isAuthenticated, CadastroServController);
+router.post("/agendamento", isAuthenticated, AppointmentController)
 
-router.post("/Servicos", isAuthenticated, CadastroServController)
-router.get("/Servicos", ListServicesController)
+router.get("/Servicos", ListServicesController);
 
 
 export {router};
