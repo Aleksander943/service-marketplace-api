c8c9b38be072934a05ddfb83ed9bff9e14a33515
Aleksander943 <aleksandededeus943@gmail.com>
2026-04-22 08:23:51 -0300

    refactor: normaliza padrao de rotas para minusculo em services e servicos

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -2,19 +2,19 @@ import { Router } from "express";
 import 'dotenv/config';
 import { CreateUserController } from "../controllers/CreateUserController";
 import { AuthUserController } from "../controllers/AuthUserController";
-import { CadastroServController } from "../controllers/CreateSeviController";
+import { CadastroServController } from "../controllers/CreateServiceController";
 import { isAuthenticated } from "../middleware/isAuthenticated";
 import { ListServicesController } from "../controllers/ListServicesController";
-import { AppointmentController } from "../controllers/AppointmentController";
+import { AppointmentController } from "../controllers/CreateAppointmentController";
 
 const router = Router();
 
 router.post("/cadastro", CreateUserController);
 router.post("/login", AuthUserController);
-router.post("/Servicos", isAuthenticated, CadastroServController);
+router.post("/servicos", isAuthenticated, CadastroServController);
 router.post("/agendamento", isAuthenticated, AppointmentController)
 
-router.get("/Servicos", ListServicesController);
+router.get("/services", ListServicesController);
 
 
 export {router};
