478687361cedebf623e033adc685cf4992de98d6
Aleksander_De_Deus <aleksandededeus943@gmail.com>
2026-05-11 15:46:05 -0300

    feat: cria rota de edição e listar informações de perfil do usuário

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -7,6 +7,8 @@ import { isAuthenticated } from "../middleware/isAuthenticated";
 import { ListServicesController } from "../controllers/ListServicesController";
 import { AppointmentController } from "../controllers/CreateAppointmentController";
 import { StatusAppointmentController } from "../controllers/StatusAppointmentController";
+import { InfoUserController } from "../controllers/InfoUserController";
+import { GetUserProfileController } from "../controllers/ListUserController";
 
 const router = Router();
 
@@ -15,8 +17,10 @@ router.post("/login", AuthUserController);
 router.post("/servicos", isAuthenticated, CadastroServController);
 router.post("/agendamento", isAuthenticated, AppointmentController)
 
+router.patch("/perfil", isAuthenticated, InfoUserController);
+
 router.get("/services", ListServicesController);
 router.get("/appointments/me", isAuthenticated, StatusAppointmentController);
-
+router.get("/perfil", isAuthenticated, GetUserProfileController )
 
 export {router};
